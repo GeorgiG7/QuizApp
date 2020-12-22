3623af40340df04a8508c5e1ea433d004bf96159
GoSh069 <gogogospodinov69@gmail.com>
2020-12-22 13:40:19 +0200

    dagger improvements

--- app/src/main/java/com/example/quizapp/di/modules/ActivityModule.java
+++ app/src/main/java/com/example/quizapp/di/modules/ActivityModule.java
@@ -1,4 +1,13 @@
 package com.example.quizapp.di.modules;
 
-public class ActivityModule {
+import com.example.quizapp.view.activities.MainActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityModule {
+
+    @ContributesAndroidInjector
+    abstract MainActivity bindMainActivity();
 }
