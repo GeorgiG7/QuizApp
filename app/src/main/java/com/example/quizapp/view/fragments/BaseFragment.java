f05bb827523e408a6181cdb8c23ba7ded3033049
GoSh069 <gogogospodinov69@gmail.com>
2020-12-22 12:02:09 +0200

    add abstract base fragment

--- app/src/main/java/com/example/quizapp/view/fragments/BaseFragment.java
+++ app/src/main/java/com/example/quizapp/view/fragments/BaseFragment.java
@@ -1,4 +1,23 @@
 package com.example.quizapp.view.fragments;
 
-public class BaseFragment {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import dagger.android.support.DaggerFragment;
+
+public abstract class BaseFragment extends DaggerFragment {
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        onFragmentCreated(view, savedInstanceState);
+    }
+
+
+    protected abstract void onFragmentCreated(View view, Bundle savedInstanceState);
+
 }
