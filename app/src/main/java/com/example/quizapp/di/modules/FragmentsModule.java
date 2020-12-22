33127b6c94be002af81a9dcf929581f7bc7fb67f
GoSh069 <gogogospodinov69@gmail.com>
2020-12-22 13:58:28 +0200

    added FragmentsModule

---
+++ app/src/main/java/com/example/quizapp/di/modules/FragmentsModule.java
@@ -0,0 +1,19 @@
+package com.example.quizapp.di.modules;
+
+import com.example.quizapp.view.fragments.QuizFragment;
+import com.example.quizapp.view.fragments.ScoreboardFragment;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class FragmentsModule {
+
+    @ContributesAndroidInjector
+    abstract QuizFragment providesQuizFragment();
+
+    @ContributesAndroidInjector
+    abstract ScoreboardFragment providesScoreBoardFragment();
+}
