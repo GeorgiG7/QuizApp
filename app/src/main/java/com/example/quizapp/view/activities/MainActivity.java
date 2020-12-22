0995a4e699f583eb8a84250c5846ed229f6e5394
GoSh069 <gogogospodinov69@gmail.com>
2020-12-22 11:20:52 +0200

    add abstract base activity

---
+++ app/src/main/java/com/example/quizapp/view/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.quizapp.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quizapp.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onViewCreated() {
+
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_main;
+    }
+}
