2316b8b7332e45273b6fe55856fe9c22e9c8c473
GoSh069 <gogogospodinov69@gmail.com>
2020-12-28 10:19:13 +0200

    example of api, still thinking if Quiz class is required

---
+++ app/src/main/java/com/example/quizapp/model/Question.java
@@ -0,0 +1,27 @@
+package com.example.quizapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Question {
+
+    @SerializedName("question")
+    String question;
+    @SerializedName("correct_answer")
+    String correctAnswer;
+    @SerializedName("incorrect_answer")
+    List<String> incorrectAnswers;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public List<String> getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+}
