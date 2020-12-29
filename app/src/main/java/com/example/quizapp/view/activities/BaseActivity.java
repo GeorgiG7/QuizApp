92016e5425dfe19c4878ff3854d33d278c44a0d1
GoSh069 <gogogospodinov69@gmail.com>
2020-12-29 12:35:13 +0200

    replaced DataBinding with ViewBinding due to unusefull functionallity of DataBinding

--- app/src/main/java/com/example/quizapp/view/activities/BaseActivity.java
+++ app/src/main/java/com/example/quizapp/view/activities/BaseActivity.java
@@ -1,22 +1,29 @@
 package com.example.quizapp.view.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.viewbinding.ViewBinding;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public abstract class BaseActivity extends DaggerAppCompatActivity {
+public abstract class BaseActivity<T extends ViewBinding> extends DaggerAppCompatActivity {
+
+    protected T binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutRes());
+
+        binding = inflateBinding();
+        View view = binding.getRoot();
+        setContentView(view);
         onViewCreated();
     }
 
-    protected abstract int getLayoutRes();
+
+    protected abstract T inflateBinding();
 
     protected abstract void onViewCreated();
 }
--- app/src/main/java/com/example/quizapp/view/activities/MainActivity.java
+++ app/src/main/java/com/example/quizapp/view/activities/MainActivity.java
@@ -2,20 +2,21 @@ package com.example.quizapp.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.quizapp.R;
+import com.example.quizapp.databinding.ActivityMainBinding;
 import com.example.quizapp.view.fragments.QuizFragment;
 import com.example.quizapp.view.fragments.ScoreboardFragment;
 
 import javax.inject.Inject;
 
-public class MainActivity extends BaseActivity {
-
-    Button btn;
+public class MainActivity extends BaseActivity<ActivityMainBinding>{
 
     @Inject
     QuizFragment quizFragment;
@@ -25,24 +26,11 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onViewCreated() {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frag_host, quizFragment);
-        fragmentTransaction.commit();
-        btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frag_host, scoreboardFragment);
-                fragmentTransaction.commit();
-            }
-        });
+
     }
 
     @Override
-    protected int getLayoutRes() {
-        return R.layout.activity_main;
+    protected ActivityMainBinding inflateBinding() {
+        return ActivityMainBinding.inflate(LayoutInflater.from(this));
     }
 }
